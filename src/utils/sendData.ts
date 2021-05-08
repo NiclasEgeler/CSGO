752859054597a9282064ec6846a7b15bd18303fc
NiclasEgeler <70487423+NiclasEgeler@users.noreply.github.com>
2021-05-08 23:20:16 +0200

    add timeout

--- src/utils/sendData.ts
+++ src/utils/sendData.ts
@@ -1,62 +1,26 @@
-import { Reader } from './reader.ts'
-
-
 export async function sendData(
   data: Uint8Array,
   ip: string,
   port: number,
-): Promise<number> {
+): Promise<Uint8Array> {
   const socket = await Deno.listenDatagram({
     port: 0,
     transport: "udp",
     hostname: "0.0.0.0",
   });
 
-  //const add = Deno.NetAddr = {transport: "udp", port: 8125, hostname: "1.2.3.4"};
-  //NetAddr
-  socket.receive().then(([buffer, addr]) => {
-      var reader = new Reader(buffer);
-      reader.readByte();
-      reader.readByte();
-      reader.readString();
-      reader.readString();
-      reader.readString();
-      reader.readString();
+  const retPromise = new Promise<Uint8Array>((resolve, reject) => {
+    var timeout = setTimeout(
+      () => reject("Source server did not respond in time"),
+      1000,
+    );
+    socket.receive().then(([buffer, addr]) => {
+      clearTimeout(timeout);
+      resolve(buffer);
+    });
   });
 
   socket.send(data, { transport: "udp", port: port, hostname: ip });
 
-  await console.log(
-    data.toString() + " " + ip.toString() + " " + port.toString(),
-  );
-
-  return -1;
-  // var socket = createSocket('udp4');
-  //     socket.send(data, port, ip, (err) => {
-  //         if (err) {
-  //             socket.close();
-  //             console.log(err)
-  //         }
-  //     });
-
-  // return new Promise((resolve, reject) => {
-  //     var wait = setTimeout(() => {
-  //         reject();
-  //     }, 300);
-
-  //     socket.on('error', (err) => {
-  //         console.log(err)
-  //         console.log('Closing socket...')
-  //         socket.close();
-  //     })
-
-  //     socket.on('message', (msg: Buffer, rinfo: RemoteInfo) => {
-  //         clearTimeout(wait);
-  //         resolve({
-  //             buffer: msg,
-  //             remoteInfo: rinfo
-  //         });
-  //         socket.close();
-  //     });
-  // })
+  return retPromise;
 }
