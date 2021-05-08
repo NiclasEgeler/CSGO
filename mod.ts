730215f76cc389bf53e7dea2e65db48019b2a25e
NiclasEgeler <70487423+NiclasEgeler@users.noreply.github.com>
2021-05-08 23:20:21 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-export { A2S_INFO, A2S_PLAYER, BASE } from "./src/models/buffer.ts";
+import { ServerInfo } from "./src/models/info.ts";
 export { getInfo } from "./src/functions/getInfo.ts";
 export { getPlayers } from "./src/functions/getPlayers.ts";
 export { getRules } from "./src/functions/getRules.ts";
