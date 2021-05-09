4ab8b96351a0902cb507fe898209aad1e8287465
NiclasEgeler <70487423+NiclasEgeler@users.noreply.github.com>
2021-05-09 16:25:00 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,3 @@
-import { ServerInfo } from "./src/models/info.ts";
 export { getInfo } from "./src/functions/getInfo.ts";
 export { getPlayers } from "./src/functions/getPlayers.ts";
 export { getRules } from "./src/functions/getRules.ts";
